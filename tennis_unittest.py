7ec105aeaf9aae0138f7638476e2f2e996b45d90
Reuben Stump <reuben.stump@servicenow.com>
2016-08-31 19:09:47 -0400

    STRY0010043 unit test failure on new feature

--- tennis_unittest.py
+++ tennis_unittest.py
@@ -49,8 +49,8 @@ test_cases = [
     (6, 5, 'Advantage One', 'One', 'player2'),
     (5, 6, 'Advantage Two', 'player1', 'Two'), 
 
-    # Test success - STRY0010043
-    #(0, 0, 'Win for player2', 'player1', 'player2'),
+    # Test failure - STRY0010043
+    (0, 0, 'Win for player2', 'player1', 'player2'),
     
     ]
 
